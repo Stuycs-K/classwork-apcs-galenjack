86cd7a128c4f4335dbf22bf90c918ce46216cb52
Galen Jack <galenj@nycstudents.net>
2024-09-27 10:00:23 -0500

    fixed issues with MyArray

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,1 +1,16 @@
-public class MyArrays 
+public class MyArrays {
+  public static String arrayToString(int[] nums) {
+    String str = "[";
+    for (int i = 0; i < nums.length; i++) {
+      str+=nums[i];
+      if (i != nums.length - 1) {
+        str+=", ";
+      }
+    }
+    return (str + "]");
+  }
+  public static void main(String[] args) {
+    int[] nums = {1,2,3,4,5};
+    System.out.println(arrayToString(nums));
+  }
+}
