7e44b6182ed89d486a4120dc26793f8f006cbea6
Galen Jack <galenj@nycstudents.net>
2024-10-15 11:00:01 -0400

    added equilateral test and confirmed that it worked for all three points

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -5,16 +5,27 @@ public class Demo{
   to the others.
   */
   public static double distance(Point a, Point b){
-    return Math.sqrt(Math.pow((a.getX() - b.getY()), 2) + Math.pow((a.getY() - b.getY()), 2));
+    return Math.sqrt(Math.pow((a.getX() - b.getX()), 2) + Math.pow((a.getY() - b.getY()), 2));
   }
 
   public static void main(String[]args){
     Point p1 = new Point(1,1);
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
+    Point p4 = new Point(0,0);
+    Point p5 = new Point(1,0);
+    Point p6 = new Point(0.5,Math.sqrt(3)/2);
     System.out.println(p3);
     System.out.println(distance(p1,p2));
     System.out.println(Point.distance(p1,p2));
     System.out.println(p1.distanceTo(p2));
+    System.out.println("Testing equilateral triangle: point 1: " + p4 + "point 2: " + p5 + "point 3: " + p6);
+    System.out.println(distance(p4,p5));
+    System.out.println(Point.distance(p5,p6));
+    System.out.println(p4.distanceTo(p6));
+
+
+
+
   }
 }
