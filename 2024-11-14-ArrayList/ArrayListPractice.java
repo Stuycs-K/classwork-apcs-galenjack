ea1ad2637e5bbfc1a4bd5ef77c6b718ebc8d1780
Galen Jack <galenj@nycstudents.net>
2024-11-14 10:49:31 -0500

    wrote replace and reversed

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -4,7 +4,7 @@ public class ArrayListPractice {
     String addVal = "";
     ArrayList<String> arr = new ArrayList<String>();
     for (int i = 0; i < size; i++) {
-      addVal = ""+(int)(Math.random()*10);
+      addVal = ""+(int)(Math.random()*11);
       if (addVal.equals("0")) {
         addVal = "";
       }
@@ -12,7 +12,28 @@ public class ArrayListPractice {
     }
     return arr;
   }
+
+public static void replaceEmpty(ArrayList<String> original){
+  for(int i = 0; i < original.size(); i++){
+    if(original.get(i).equals("")){
+      original.set(i, "Empty");
+    }
+  }
+}
+
+public static ArrayList<String> makeReversedList(ArrayList<String> original){
+  ArrayList<String> ret = new ArrayList<String>();
+  for(int i = original.size() - 1; i >= 0; i--){
+    ret.add(original.get(i));
+  }
+  return ret;
+}
+
   public static void main(String[]args) {
-    System.out.print(createRandomArray(10));
+    ArrayList<String> test = createRandomArray(10);
+    replaceEmpty(test);
+    System.out.print(test);
+    test = makeReversedList(test);
+    System.out.print(test);
   }
 }
