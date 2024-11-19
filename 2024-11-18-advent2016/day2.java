61173fa97a760f2aa9114276cc8ca2edd2524ee6
Galen Jack <galenj@nycstudents.net>
2024-11-19 11:01:16 -0500

    started working on day2

---
+++ 2024-11-18-advent2016/day2.java
@@ -0,0 +1,13 @@
+public class day2{
+  public static int solve(String[][] input) {
+    int[][] padArray = {
+      {1, 2, 3}, {4, 5, 6}, {7, 8, 9}
+    };
+    int[] cursor = {1, 1};
+    for (int i = 0; i < input.length; i++) {
+      for int (n = 0; n < input[i].length; n++){
+        if (input[])
+      }
+    }
+  }
+}
