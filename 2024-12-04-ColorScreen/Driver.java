56249413bbfe9699db24ccf2ebf317b8d90df0f9
Galen Jack <gjack60@stuy.edu>
2024-12-04 16:47:51 -0500

    added drawing

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import java.util.*;
 public class Driver{
     public static void main(String[]args){
@@ -11,8 +15,49 @@ public class Driver{
             Text.go(3, i);
             System.out.print("*");
         }
+        Text.color(0);
+        printAscii();
+
+
+
         Text.go(31,0);
     }
+
+    public static void sleep(int milli){
+      try{
+              Thread.sleep(milli);
+      }catch(Exception e){
+      }
+}
+    
+    public static void printAscii() {
+    try{
+      File file = new File("ascii.txt");
+      Scanner input = new Scanner(file);
+
+      int count = 5;
+      while(input.hasNextLine()) {
+        Text.go(count,3);
+        String line = input.nextLine();
+        Scanner lineScan = new Scanner(line);
+        
+        System.out.print(line);
+        count++;
+        sleep(30);
+      }
+
+      input.close();
+
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+
+    }
+  }
+
+
+
+
+
     public static void border(){
         Text.color(Text.background(36));
         for (int i = 0; i < 80; i++){
